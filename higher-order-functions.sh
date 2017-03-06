6ed6ffcb755e226be1fb0856a526899f8e719524
Craig Buchek <github@craigbuchek.com>
2017-03-06 13:35:33 -0600

    Add `ignore_errors`
    This is something I've wanted for a couple weeks. You could just add `|| true` to the end of a command, but sometimes this is more readable.

--- higher-order-functions.sh
+++ higher-order-functions.sh
@@ -7,3 +7,18 @@ function filter() {
 function reject() {
     xargs -I{} sh -c "$@ '{}' || echo '{}'"
 }
+
+# Run a command, ignoring errors temporarily. Alternatively, give not options to ignore all errors going forward, or `--off` to terminate on errors going forward.
+function ignore_errors() {
+    if [ "$@" == "" ]; then
+        set +e
+    elif [ "$@" == "--off" ]; then
+        set -e
+    elif echo $SHELLOPTS | grep -q 'errexit' ; then
+        set +e
+        $@
+        set -e
+    else
+        $@
+    fi
+}
